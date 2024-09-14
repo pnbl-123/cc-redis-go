e0055881ed565699cee79d17a502f18c894b7325
Phung Ng Bao Long <phungngbaolong@gmail.com>
2024-09-14 23:47:33 +0700

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -2,34 +2,40 @@ package main
 
 import (
 	"fmt"
-	"os"
-
+	"io"
 	"net"
+	"os"
 )
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	defer l.Close()
+
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	defer conn.Close()
-	// read datasource, init reader
-
-	for i := 0; i < 2; i++ {
-		// write
-		_, err = conn.Write([]byte("+PONG\r\n"))
-
-		if err != nil {
-			fmt.Println("Error write msg: ", err.Error())
-			os.Exit(1)
-		}
+	if err != nil {
+		fmt.Println("Error parsing command: ", err.Error())
+		os.Exit(1)
 	}
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+
+	for {
+		_, err := conn.Read(buf)
+		if err == io.EOF {
+			os.Exit(0)
+		}
+
+		conn.Write([]byte("+PONG\r\n"))
+	}
+
 }
